cdd86b68a33e2181d704749b7b01d03e8c8e2aac
anekrasova210804 <a.nekrasova1@uniyar.ac.ru>
2024-05-14 13:20:13 +0300

    New draft!

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import java.util.concurrent.Semaphore;
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
+    private static final int NUM_PHILOSOPHERS = 5;
+    public static void main(String[] args) throws InterruptedException {
         var frame = new JFrame();
         var icon = new ImageIcon("philosophers.png");
         var label = new JLabel(icon);
@@ -13,8 +15,19 @@ public class Main {
                 (JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        Semaphore sem = new Semaphore(2);
-        for(int i=1;i<6;i++)
-            new Philosopher(sem,i).start();
+
+        Semaphore[] chopsticks = new Semaphore[NUM_PHILOSOPHERS];
+
+        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+            chopsticks[i] = new Semaphore(1);
         }
+
+        // Create the philosophers and start each running in its own thread.
+        Philosopher[] philosophers = new Philosopher[NUM_PHILOSOPHERS];
+
+        for (int i = 0; i < NUM_PHILOSOPHERS; i++) {
+            philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % NUM_PHILOSOPHERS]);
+            new Thread(philosophers[i]).start();
+        }
+    }
 }
